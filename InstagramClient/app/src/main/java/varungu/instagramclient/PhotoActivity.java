452a14507e05aa99c292c8ab3aca7e10ed1f4823
varun <varungupta@live.com>
2015-05-10 21:57:42 -0700

    Allow user to Share photos from photo activity.

--- InstagramClient/app/src/main/java/varungu/instagramclient/PhotoActivity.java
+++ InstagramClient/app/src/main/java/varungu/instagramclient/PhotoActivity.java
@@ -1,7 +1,15 @@
 package varungu.instagramclient;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore.Images;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -15,7 +23,7 @@ public class PhotoActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
-        ImageView ivImage = (ImageView) findViewById(R.id.ivImage);
+        final ImageView ivImage = (ImageView) findViewById(R.id.ivImage);
 
         Bundle extras = getIntent().getExtras();
         String url = extras.getString("url");
@@ -27,6 +35,13 @@ public class PhotoActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_photo, menu);
+        MenuItem item = menu.findItem(R.id.miShare);
+        ShareActionProvider miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        // Since picasso caches images, this image should already be loaded from main screen.
+        ImageView ivImage = (ImageView) findViewById(R.id.ivImage);
+        setupShareIntent(ivImage, miShareAction);
+
         return true;
     }
 
@@ -43,5 +58,30 @@ public class PhotoActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // Gets the image URI and setup the associated share intent to hook into the provider
+    public void setupShareIntent(ImageView ivImage, ShareActionProvider miShareAction) {
+        // Fetch Bitmap Uri locally
+        Uri bmpUri = getLocalBitmapUri(ivImage); // see previous remote images section
+        // Create share intent as described above
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        // Attach share event to the menu item provider
+        miShareAction.setShareIntent(shareIntent);
+    }
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(ImageView ivImage) {
+        Drawable mDrawable = ivImage.getDrawable();
+        Bitmap mBitmap = ((BitmapDrawable)mDrawable).getBitmap();
+
+        String path = Images.Media.insertImage(getContentResolver(),
+                mBitmap, "Image Description", null);
+
+        Uri uri = Uri.parse(path);
+        return uri;
     }
 }
